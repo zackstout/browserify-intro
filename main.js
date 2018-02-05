95a9f7b3c48f2fd9b0aa8dae22f47fdef14b0864
Zack Stout <zackstout@gmail.com>
2018-02-05 14:47:01 -0600

    wooooo, we finally got there: lighting the scene

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 "use strict";
 
-var R = require('ramda');
-
-var square = function square (x) { return x * x; };
-var squares = R.chain(square, [1, 2, 3, 4, 5]);
+// var R = require('ramda');
+//
+// var square = function square (x) { return x * x; };
+// var squares = R.chain(square, [1, 2, 3, 4, 5]);
 
 // document.getElementById('response').innerHTML = squares;
+
+
+// var loader = new THREE.BinaryLoader();
+// var callback = function( geometry ) {
+//   object = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial( { color: 0x555555, specular: 0x111111, shininess: 50 }  )  );
+//   object.scale.x = object.scale.y = object.scale.z = 0.80;
+//   scene.add( object );
+// };
+// loader.load( "obj/walt/WaltHead_bin.js", callback );
+
+
+
 
 var THREE = require('three');
 var OrbitControls = require('three-orbit-controls')(THREE)
@@ -21,12 +33,27 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+for (var i=0; i < 10; i ++) {
+  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+  var cube = new THREE.Mesh( geometry, material );
+  cube.position.y = i * 1.2;
+  scene.add( cube );
+}
+
 
 camera.position.z = 5;
+
+
+// var sphere = new THREE.SphereGeometry( 5, 5, 10 );
+// var light1 = new THREE.PointLight( 0xff0040, 2, 50 );
+// light1.add( new THREE.Mesh( sphere, new THREE.MeshLambertMaterial( { color: 0xff0040 } ) ) );
+// scene.add( light1 );
+// light1.position.y = 10;
+
+var light = new THREE.PointLight(0xffffff);
+light.position.set(-10,20,10);
+scene.add(light);
 
 var animate = function () {
   requestAnimationFrame( animate );
